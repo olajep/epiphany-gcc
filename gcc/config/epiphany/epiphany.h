42e6cd9cccbb4eef320144aa416a6ff730009715
Joern Rennecke <joern.rennecke@embecosm.com>
2013-08-01 17:06:19 +0100

    epiphany: Use need_trace instead of TRACE_REGNUM
    gcc/
    	Backout these changes:
    	* config/epiphany/epiphany.h (FIRST_PSEUDO_REGISTER): Bump up to 79.
    	(FIXED_REGISTERS, CALL_USED_REGISTERS): Include TRACE_REGNUM entry.
    	(REG_ALLOC_ORDER, REGISTER_NAMES): Likewise.
    	(REG_CLASS_CONTEENTS) <ALL_REGS>: Likewise.
    	* config/epiphany/epiphany.md (TRACE_REGNUM): New constant.
    	(sibcall, sibcall_i): Add use of trace.
    	(sibcall_value, sibcall_value_i): Likewise.
    	* config/epiphany/epiphany.c (epiphany_initial_elimination_offset):
    	Define elimination from TRACE_REGNUM.
    	(epiphany_can_eliminate): Only restrict elimination from
    	TRACE_REGNUM.
    
    	* config/epiphany/epiphany-protos.h (epiphany_need_fp): Declare.
    	* config/epiphany/epiphany.c (struct epiphany_frame_info):
    	Change need_fp to bool.
    	New member need_trace.
    	(epiphany_compute_frame_size): Set / use need_trace member.
    	Avoid GPR_LR in last_slot.
    	(piphany_initial_elimination_offset): Handle elimination of
    	HARD_FRAME_POINTER_REGNUM.
    	(epiphany_can_eliminate): Likewise.
    	* config/epiphany/epiphany.h (machine_function): Add member
    	expanded_non_sibcall.
    	(ELIMINABLE_REGS): Add entry for HARD_FRAME_POINTER_REGNUM.
    	* config/epiphany/epiphany.md (reload_insi_ra): Use epiphany_need_fp.
    	(call_i, call_value_i): Use (mem:BLK (reg:SI GPR_FP)) instead
    	of TRACE_REGNUM.
    	(call, call_value): Likewise.
    	Set MACHINE_FUNCTION (cfun)->expanded_non_sibcall.
    	* config/epiphany/predicates.md (trace_operand): Check for MEM.
    
    Reworded-by: Ola Jeppsson <ola.jeppsson@gmail.com>

--- gcc/config/epiphany/epiphany.h
+++ gcc/config/epiphany/epiphany.h
@@ -210,7 +210,7 @@ along with GCC; see the file COPYING3.  If not see
    All registers that the compiler knows about must be given numbers,
    even those that are not normally considered general registers.  */
 
-#define FIRST_PSEUDO_REGISTER 79
+#define FIRST_PSEUDO_REGISTER 78
 
 
 /* General purpose registers.  */
@@ -245,7 +245,6 @@ along with GCC; see the file COPYING3.  If not see
 	1, 1, 1, 1, 1, 1,               /* Core Control Registers.  */  \
 	1, 1, 1,			/* FP_{NEAREST,...}_REGNUM */\
 	1,				/* UNKNOWN_REGNUM - placeholder.  */\
-	1,				/* TRACE_REGNUM.  */\
 }
 
 /* Like `FIXED_REGISTERS' but has 1 for each register that is clobbered (in
@@ -274,7 +273,6 @@ along with GCC; see the file COPYING3.  If not see
 	1, 1, 1, 1, 1, 1,               /* Core Control Registers.  */  \
 	1, 1, 1,			/* FP_{NEAREST,...}_REGNUM */\
 	1,				/* UNKNOWN_REGNUM - placeholder.  */\
-	1,				/* TRACE_REGNUM.  */\
 }
 
 #define REG_ALLOC_ORDER \
@@ -293,7 +291,7 @@ along with GCC; see the file COPYING3.  If not see
     14, 13, /* Link register, stack pointer.  */ \
     /* Can't allocate, but must name these... */ \
     28, 29, 30, 31, \
-    64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78 \
+    64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77 \
   }
 
 #define HARD_REGNO_RENAME_OK(SRC, DST) epiphany_regno_rename_ok (SRC, DST)
@@ -360,7 +358,7 @@ enum reg_class {
   { 0xffff100f,0xffffff00,0x0},  /* SIBCALL_REGS */			\
   { 0xffffffff,0xffffffff,0x0003}, /* GENERAL_REGS */			\
   { 0x00000000,0x00000000,0x03f0}, /* CORE_CONTROL_REGS */		\
-  { 0xffffffff,0xffffffff,0x7fff}, /* ALL_REGS */				\
+  { 0xffffffff,0xffffffff,0x3fff}, /* ALL_REGS */				\
 }
 
 
@@ -425,6 +423,7 @@ typedef struct GTY (()) machine_function
   unsigned lr_clobbered : 1;
   unsigned control_use_inserted : 1;
   unsigned lr_slot_known : 1;
+  unsigned expanded_non_sibcall : 1;
   unsigned sw_entities_processed : 6;
   long lr_slot_offset;
   rtx and_mask;
@@ -479,7 +478,7 @@ typedef struct GTY (()) machine_function
 #define ELIMINABLE_REGS						\
 {{ FRAME_POINTER_REGNUM, STACK_POINTER_REGNUM},			\
  { FRAME_POINTER_REGNUM, HARD_FRAME_POINTER_REGNUM},		\
- { TRACE_REGNUM, HARD_FRAME_POINTER_REGNUM},		\
+ { HARD_FRAME_POINTER_REGNUM, STACK_POINTER_REGNUM},		\
  { ARG_POINTER_REGNUM, STACK_POINTER_REGNUM},                   \
  { ARG_POINTER_REGNUM, HARD_FRAME_POINTER_REGNUM},		\
 }
@@ -725,7 +724,7 @@ typedef struct GTY (()) machine_function
   "r56", "r57", "r58", "r59", "r60", "r61", "r62", "r63",	\
   "ap",  "sfp", "cc1", "cc2",					\
   "config", "status", "lc", "ls", "le", "iret",			\
-  "fp_near", "fp_trunc", "fp_anyfp", "unknown", "trace"		\
+  "fp_near", "fp_trunc", "fp_anyfp", "unknown"			\
 }
 
 #define FINAL_PRESCAN_INSN(INSN, OPVEC, NOPERANDS) \
